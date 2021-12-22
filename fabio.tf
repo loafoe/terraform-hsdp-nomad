df29fb328b80355c790de38d07a212644a7cf62e
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-12-22 11:12:22 +0100

    We need the proxy

--- fabio.tf
+++ fabio.tf
@@ -8,4 +8,6 @@ resource "nomad_job" "fabio" {
   jobspec = templatefile("${path.module}/templates/fabio.nomad.tpl", {
     CONSUL_REGISTRY_ADDR = "http://${hsdp_container_host.nomad_server.private_ip}:4040"
   })
+
+  depends_on = [cloudfoundry_app.nomad_proxy]
 }
